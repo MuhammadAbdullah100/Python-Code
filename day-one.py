d2b5c122a3bf841268f7833cde7b136214e57cdd
Muhammad Abdullah <46645651+MuhammadAbdullah100@users.noreply.github.com>
2020-04-25 02:23:24 +0500

    pythonCode
    i code a python from variables to if or else if statement

---
+++ day-one.py
@@ -0,0 +1,163 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Hello World")
+
+
+# In[2]:
+
+
+print("Hello Word. There is",1,"time showing HW")
+
+
+# In[5]:
+
+
+name ="Abdullah."
+sentence ="My name is"
+number = 18
+sentence2 = "I am"
+sentence3 = "years old."
+
+
+# In[6]:
+
+
+print(sentence,name,sentence2,number,sentence3)
+
+
+# In[17]:
+
+
+value1 = 2
+AddSign = "+"
+SubSign = "-"
+EqualSign = "="
+answer1 = 2+2
+answer2 = 4-2
+
+
+# In[18]:
+
+
+print(value1,AddSign,value2,EqualSign,answer1)
+
+
+# In[19]:
+
+
+print(answer1,SubSign,value1,EqualSign,answer2)
+
+
+# In[20]:
+
+
+value1 = 3
+
+
+# In[21]:
+
+
+print(value1)
+
+
+# In[22]:
+
+
+num1 = 1000
+num2 = 570
+num3 = 200
+sum= num1+num2*num3
+print(sum)
+
+
+# In[28]:
+
+
+a = 100
+b = 1100
+if a == b:
+    print("true")
+else:
+        print("false")
+
+# In[32]:
+
+
+num1 = 100
+num2 = 50
+sum1  =  num1+num2
+num3 =  50
+
+sum2 =sum1/num3
+print(sum2)
+
+
+# In[33]:
+
+
+if num1-num2 != num3:
+    print(" We are not same bro")
+else:
+    print("We are same bro")
+
+
+# In[36]:
+
+
+if num1-num2 == num3:
+    print(" We are not same bro")
+else:
+    print("We are same bro")
+
+
+# In[37]:
+
+
+if num1-num2 = num3:
+
+    print(" We are not same bro")
+else:
+    print("We are same bro")
+
+
+# In[38]:
+
+
+if num1-num2 == num3:
+    print(" We are not same bro")
+
+
+# In[42]:
+
+
+# let make a result for maths
+# student 1 secure 500 marks in maths. total marks is 501
+student1_marks = 500
+total_marks = 501
+passing_marks =500.5
+
+#let's check the student 1 are pass in maths and how much high marks he secure.
+#and if he fail. so how much marks need in maths.
+
+if student1_marks-total_marks == passing_marks:
+    print("He passed in maths", "he secured",student1_marks)
+else:
+    print("He failed.","He obtained",student1_marks,".","He need",passing_marks,"in maths");
+
+
+# In[45]:
+
+
+if student1_marks>passing_marks:
+    alert("truee")
+
+
+# In[ ]:
+
+
+
+
